022a092b1dbf2f01ff0a619e788c46cb72e5aa40
manhumang <mhumanesangel477@gmail.com>
2015-05-06 13:52:34 +0200

    Cambios varios

--- Sapys/Sapys/settings.py
+++ Sapys/Sapys/settings.py
@@ -129,6 +129,7 @@ INSTALLED_APPS = (
     'principal',
 )
 
+TEST_RUNNER = 'django.test.simple.DjangoTestSuiteRunner'
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
