f90c56c9249c72e39b6078416c968d9d2cde382b
diegogalocha <galocha15>
2015-05-12 18:04:24 +0200

    Commit con algunos formularios y listar.

--- Sapys/Sapys/urls.py
+++ Sapys/Sapys/urls.py
@@ -4,6 +4,9 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^$','principal.views.index'),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^appointments/','principal.views.appointmentList'),
+    url(r'^appointment/new/','principal.views.new_appointment'),
 )
