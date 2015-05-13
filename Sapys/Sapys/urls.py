4359833363702b4318afae7900d5a00efacc438a
manhumang <mhumanesangel477@gmail.com>
2015-05-13 14:47:31 +0200

    Primera version de funcionalidad login incluida

--- Sapys/Sapys/urls.py
+++ Sapys/Sapys/urls.py
@@ -4,9 +4,12 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$','principal.views.index'),
+    
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^appointments/','principal.views.appointmentList'),
     url(r'^appointment/new/','principal.views.new_appointment'),
-)
+    url(r'^login/$','Sapys.views.login_page', name="login"),
+    url(r'^$','Sapys.views.homepage', name="homepage"),
+    url(r'^logout/$','Sapys.views.logout_view', name="logout"),
+    )
