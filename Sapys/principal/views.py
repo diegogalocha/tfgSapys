082f034eef65ff66e8b019c1c29a415fbd93d226
diegogalocha <galocha15>
2015-05-13 17:41:51 +0200

    Añadidos listar y formularios.

--- Sapys/principal/views.py
+++ Sapys/principal/views.py
@@ -11,7 +11,47 @@ from django.contrib.auth.decorators import login_required
 
 def index(request):
     return render_to_response('index.html')
-#para crear formularios:
+#para crear formularios: HAY QUE CAMBIAR LOS REDIRECTS
+def new_student(request):
+    if request.method == 'POST':
+        formulario = StudentForm(request.POST, request.FILES)
+        if formulario.is_valid():
+            formulario.save()
+            return HttpResponseRedirect('/')
+    else:
+        formulario = StudentForm()
+    return render_to_response('studentform.html', {'formulario':formulario}, context_instance = RequestContext(request))
+
+def new_notification(request):
+    if request.method == 'POST':
+        formulario = NotificationForm(request.POST, request.FILES)
+        if formulario.is_valid():
+            formulario.save()
+            return HttpResponseRedirect('/')
+    else:
+        formulario = NotificationForm()
+    return render_to_response('notificationform.html', {'formulario':formulario}, context_instance = RequestContext(request))
+
+def new_score(request):
+    if request.method == 'POST':
+        formulario = ScoreForm(request.POST, request.FILES)
+        if formulario.is_valid():
+            formulario.save()
+            return HttpResponseRedirect('/')
+    else:
+        formulario = ScoreForm()
+    return render_to_response('scoreform.html', {'formulario':formulario}, context_instance = RequestContext(request))
+
+def new_class(request):
+    if request.method == 'POST':
+        formulario = ClassForm(request.POST, request.FILES)
+        if formulario.is_valid():
+            formulario.save()
+            return HttpResponseRedirect('/')
+    else:
+        formulario = ClassForm()
+    return render_to_response('classform.html', {'formulario':formulario}, context_instance = RequestContext(request))
+
 def new_appointment(request):
     if request.method == 'POST':
         formulario = AppointmentForm(request.POST, request.FILES)
@@ -22,9 +62,95 @@ def new_appointment(request):
         formulario = AppointmentForm()
     return render_to_response('appointmentform.html', {'formulario':formulario}, context_instance = RequestContext(request))
 
+def new_subject(request):
+    if request.method == 'POST':
+        formulario = SubjectForm(request.POST, request.FILES)
+        if formulario.is_valid():
+            formulario.save()
+            return HttpResponseRedirect('/')
+    else:
+        formulario = SubjectForm()
+    return render_to_response('subjectform.html', {'formulario':formulario}, context_instance = RequestContext(request))
+
+def new_supervisor(request):
+    if request.method == 'POST':
+        formulario = SupervisorForm(request.POST, request.FILES)
+        if formulario.is_valid():
+            formulario.save()
+            return HttpResponseRedirect('/')
+    else:
+        formulario = SupervisorForm()
+    return render_to_response('supervisorform.html', {'formulario':formulario}, context_instance = RequestContext(request))
+
+def new_teacher(request):
+    if request.method == 'POST':
+        formulario = TeacherForm(request.POST, request.FILES)
+        if formulario.is_valid():
+            formulario.save()
+            return HttpResponseRedirect('/')
+    else:
+        formulario = TeacherForm()
+    return render_to_response('teacherform.html', {'formulario':formulario}, context_instance = RequestContext(request))
+
+def new_userAccount(request):
+    if request.method == 'POST':
+        formulario = UserAccountForm(request.POST, request.FILES)
+        if formulario.is_valid():
+            formulario.save()
+            return HttpResponseRedirect('/')
+    else:
+        formulario = UserAccountForm()
+    return render_to_response('useraccountform.html', {'formulario':formulario}, context_instance = RequestContext(request))
+
+
+def new_center(request):
+    if request.method == 'POST':
+        formulario = CenterForm(request.POST, request.FILES)
+        if formulario.is_valid():
+            formulario.save()
+            return HttpResponseRedirect('/')
+    else:
+        formulario = CenterForm()
+    return render_to_response('centerform.html', {'formulario':formulario}, context_instance = RequestContext(request))
+
+
 #para listar
+def userAccountList(request):
+    userAccounts = UserAccount.objects.all()
+    return render_to_response('userAccountList.html',{'list':userAccounts})
+
+def studentList(request):
+    students = Student.objects.all()
+    return render_to_response('studentList.html',{'list':students})
+
+def notificationList(request):
+    notifications = Notification.objects.all()
+    return render_to_response('notificationList.html',{'list':notifications})
+
+def scoreList(request):
+    scores = Score.objects.all()
+    return render_to_response('scoreList.html',{'list':scores})
+def classList(request):
+    classes = Class.objects.all()
+    return render_to_response('classList.html',{'list':classes})
+
+def subjectList(request):
+    subjects = Subject.objects.all()
+    return render_to_response('subjectList.html',{'list':subjects})
+
+def supervisorList(request):
+    supervisors = Supervisor.objects.all()
+    return render_to_response('supervisorList.html',{'list':supervisors})
+
+def teacherList(request):
+    teachers = Teacher.objects.all()
+    return render_to_response('teacherList.html',{'list':teachers})
 
 def appointmentList(request):
     appointments = Appointment.objects.all()
     return render_to_response('appointmentList.html',{'list':appointments})
+
+def centerList(request):
+    centers = Center.objects.all()
+    return render_to_response('centerList.html',{'list':centers})
 
