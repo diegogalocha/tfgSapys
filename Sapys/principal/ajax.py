c29d1c860e59b426e7b60344ef7578dec4e458fa
diegogalocha <galocha15>
2015-07-08 00:40:31 +0200

    Pequeños retoques y pruebas.

--- Sapys/principal/ajax.py
+++ Sapys/principal/ajax.py
@@ -2,19 +2,28 @@
 from dajax.core import Dajax
 from dajaxice.decorators import dajaxice_register
 
-def multiplay(request, a, b):
-    dajax = Dajax()
-    result = int(a) + int(b)
-    dajax.assign('#result', 'value', str(result))
-    return dajax.json()
-
 @dajaxice_register
 def updatecombo(request, option):
     dajax = Dajax()
-    options = [['SEVILLA'], ['CADIZ']]
+    options = [['Madrid', 'Barcelona', 'Vitoria', 'Burgos'],
+               ['Paris', 'Evreux', 'Le Havre', 'Reims'],
+               ['London', 'Birmingham', 'Bristol', 'Cardiff']]
     out = []
     for option in options[int(option)]:
         out.append("<option value='#'>%s</option>" % option)
         
     dajax.assign('combo2', 'innerHTML', ''.join(out))
+    return dajax.json()
+
+@dajaxice_register
+def updatecombo2(request, option):
+    dajax = Dajax()
+    options = [['Madrid', 'Barcelona', 'Vitoria', 'Burgos'],
+               ['Paris', 'Evreux', 'Le Havre', 'Reims'],
+               ['London', 'Birmingham', 'Bristol', 'Cardiff']]
+    out = []
+    for option in options[int(option)]:
+        out.append("<option value='#'>%s</option>" % option)
+
+    dajax.assign('#combo2', 'innerHTML', ''.join(out))
     return dajax.json()
